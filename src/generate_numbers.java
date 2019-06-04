9967df18294a36a46a614f51a899327d54e1cd6c
Prashanth Kumar Purshotam <23168694+praxpk@users.noreply.github.com>
2019-06-04 11:08:45 -0400

    Initial commit

---
+++ src/generate_numbers.java
@@ -0,0 +1,11 @@
+public class generate_numbers {
+    private int capacity;
+    private int [] array_of_numbers;
+    public generate_numbers(int capacity, int option){
+        this.capacity = capacity;
+        array_of_numbers = new int[capacity];
+        if(option==1){
+
+        }
+    }
+}
