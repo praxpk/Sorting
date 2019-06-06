54637e0f299d631e3ea42e39233cc22416a0e6cf
Prashanth Kumar Purshotam <23168694+praxpk@users.noreply.github.com>
2019-06-06 15:14:44 -0400

    code to shuffle numbers and generate a list of random numbers.

--- src/generate_numbers.java
+++ src/generate_numbers.java
@@ -1,11 +1,31 @@
-public class generate_numbers {
-    private int capacity;
-    private int [] array_of_numbers;
-    public generate_numbers(int capacity, int option){
-        this.capacity = capacity;
-        array_of_numbers = new int[capacity];
-        if(option==1){
+import java.util.Arrays;
 
+public class generate_numbers {
+    private int[] array_of_numbers;
+
+    public generate_numbers(int capacity, int option) {
+        array_of_numbers = new int[capacity];
+        if (option == 1) {
+            for (int i = 0; i < capacity; i++) {
+                array_of_numbers[i] = i;
+            }
+            shuffle_numbers();
+        } else if(option==2){
+            for(int i=0;i<capacity;i++){
+                array_of_numbers[i]=(int)(Math.random()*(Math.pow(10,Math.random()*10)));
+            }
         }
+    }
+
+    public void shuffle_numbers() {
+        int temp=0;
+        for(int i=0;i<array_of_numbers.length;i++){
+            int random_index = (int)(Math.random()*array_of_numbers.length);
+            temp = array_of_numbers[i];
+            array_of_numbers[i] = array_of_numbers[random_index];
+            array_of_numbers[random_index] = temp;
+        }
+        System.out.println(Arrays.toString(array_of_numbers));
+
     }
 }
