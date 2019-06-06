e728893630c874980f533e6d402dda3c5b9b242a
Prashanth Kumar Purshotam <23168694+praxpk@users.noreply.github.com>
2019-06-06 16:14:47 -0400

    generate and print array of shuffled and random numbers

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         generate_numbers set1 = new generate_numbers(100,1);
+        //generate a shuffled list of numbers from 0 to 99
         generate_numbers set2 = new generate_numbers(100,2);
+        //generate 100 random numbers.
         set1.print_array();
         set2.print_array();
+        //print the two arrays.
     }
 }
